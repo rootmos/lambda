f68047a0a5cec2ee24ced5126ac4b862fecca60a
Gustav Behm <gustav.behm@gmail.com>
2016-03-01 08:22:28 +0100

    Testing out a graph based interpretation of the lambda expressions

--- Makefile
+++ Makefile
@@ -3,3 +3,11 @@ GHC_OPTS=-Wall
 .PHONY: lambda
 lambda: lambda.hs
 	runhaskell -- $(GHC_OPTS) lambda.hs
+
+.PHONY: deps
+deps:
+	cabal install fgl
+
+.PHONY: ghci
+ghci:
+	ghci lambda.hs
--- lambda.hs
+++ lambda.hs
@@ -2,56 +2,82 @@ module Lamda where
 
 import Test.Hspec
 import Data.List
+import Data.Graph.Inductive
+import Data.Graph.Inductive.PatriciaTree
+import Control.Monad.State
 
-type VariableName = String
+type Name = String
 
-data Expr = Variable VariableName | Lambda VariableName Expr | App Expr Expr
+data AST = V Name | L Name AST | A AST AST
     deriving Show
 
-data Error = VariableNotFree VariableName Expr
+free :: AST -> [Name]
+free (V v) = [v]
+free (L v expr) = delete v $ free expr
+free (A m n) = sort . nub $ free m ++ free n
 
-subst :: VariableName -> Expr -> Either Error Expr
-subst x expr = Right expr
-
-free :: Expr -> [VariableName]
-free (Variable v) = [v]
-free (Lambda v expr) = delete v $ free expr
-free (App m n) = sort . nub $ free m ++ free n
-
-alphaEquivalent :: Expr -> Expr -> Bool
-alphaEquivalent (Variable _) (Variable _) = True
-alphaEquivalent (Lambda x e) (Lambda y f) = (x `elem` free e) == (y `elem` free f) && e `alphaEquivalent` f
+alphaEquivalent :: AST -> AST -> Bool
+alphaEquivalent (V _) (V _) = True
+alphaEquivalent (L x e) (L y f) = (x `elem` free e) == (y `elem` free f) && e `alphaEquivalent` f
 alphaEquivalent _ _ = False
+
+
+data NodeLabel = Let Name | Variable Name | Lambda Name | App
+    deriving (Show, Eq)
+data EdgeLabel = Binding | Body | Function | Argument
+    deriving (Show, Eq)
+
+type Expr = Gr NodeLabel EdgeLabel
+type ExprNode = LNode NodeLabel
+type ExprEdge = LEdge EdgeLabel
+
+variable :: Name -> State Expr ExprNode
+variable n = do
+    node <- get >>= return . head . newNodes 1
+    let lnode = (node, Variable n)
+    modify $ insNode lnode
+    return lnode 
+
+lambda :: Name -> ExprNode -> State Expr ExprNode
+lambda x body = do
+    node <- get >>= return . head . newNodes 1
+    let lambdaNode = (node, Lambda x)
+    let bodyEdge = (fst lambdaNode, fst body, Body)
+    modify $ insNode lambdaNode
+    modify $ insEdge bodyEdge
+    return lambdaNode
+
+foo = execState (variable "x" >>= lambda "y") empty
 
 main :: IO ()
 main = hspec $ do
     describe "free" $ do
         it "returns [x] for x" $ do
-            free (Variable "x") `shouldBe` ["x"]
-        it "returns [] for Lambda x x" $ do
-            free (Lambda "x" (Variable "x")) `shouldBe` []
-        it "returns [y] for Lambda x y" $ do
-            free (Lambda "x" (Variable "y")) `shouldBe` ["y"]
-        it "returns [x,y] for App x y" $ do
-            free (App (Variable "x") (Variable "y")) `shouldBe` ["x", "y"]
-        it "returns [y] for Lambda x (App x y)" $ do
-            free (Lambda "x" (App (Variable "x") (Variable "y"))) `shouldBe` ["y"]
-        it "returns [x] for Lambda y (App x y)" $ do
-            free (Lambda "y" (App (Variable "x") (Variable "y"))) `shouldBe` ["x"]
-        it "returns [y,z] for App (Lambda x z) y" $ do
-            free (App (Lambda "x" (Variable "z")) (Variable "y")) `shouldBe` ["y","z"]
-        it "returns [y,z] for App y (Lambda x z)" $ do
-            free (App (Variable "y") (Lambda "x" (Variable "z"))) `shouldBe` ["y","z"]
-        it "returns [x] for App x x" $ do
-            free (App (Variable "x") (Variable "x")) `shouldBe` ["x"]
+            free (V "x") `shouldBe` ["x"]
+        it "returns [] for L x x" $ do
+            free (L "x" (V "x")) `shouldBe` []
+        it "returns [y] for L x y" $ do
+            free (L "x" (V "y")) `shouldBe` ["y"]
+        it "returns [x,y] for A x y" $ do
+            free (A (V "x") (V "y")) `shouldBe` ["x", "y"]
+        it "returns [y] for L x (A x y)" $ do
+            free (L "x" (A (V "x") (V "y"))) `shouldBe` ["y"]
+        it "returns [x] for L y (A x y)" $ do
+            free (L "y" (A (V "x") (V "y"))) `shouldBe` ["x"]
+        it "returns [y,z] for A (L x z) y" $ do
+            free (A (L "x" (V "z")) (V "y")) `shouldBe` ["y","z"]
+        it "returns [y,z] for A y (L x z)" $ do
+            free (A (V "y") (L "x" (V "z"))) `shouldBe` ["y","z"]
+        it "returns [x] for A x x" $ do
+            free (A (V "x") (V "x")) `shouldBe` ["x"]
     describe "alphaEquivalent" $ do
         it "claims that x is alpha-equivalent to y" $ do
-            (Variable "x") `alphaEquivalent` (Variable "y") `shouldBe` True
+            (V "x") `alphaEquivalent` (V "y") `shouldBe` True
         it "claims that λx.x is alpha-equivalent to λy.y" $ do
-            (Lambda "x" (Variable "x")) `alphaEquivalent` (Lambda "y" (Variable "y")) `shouldBe` True
+            (L "x" (V "x")) `alphaEquivalent` (L "y" (V "y")) `shouldBe` True
         it "claims that λx.λx.x is not alpha-equivalent to λy.λx.y" $ do
-            (Lambda "x" (Lambda "x" (Variable "x"))) `alphaEquivalent` (Lambda "y" (Lambda "x" (Variable "y"))) `shouldBe` False
+            (L "x" (L "x" (V "x"))) `alphaEquivalent` (L "y" (L "x" (V "y"))) `shouldBe` False
         it "claims that λx.λx.x is alpha-equivalent to λy.λx.x" $ do
-            (Lambda "x" (Lambda "x" (Variable "x"))) `alphaEquivalent` (Lambda "y" (Lambda "x" (Variable "x"))) `shouldBe` True
+            (L "x" (L "x" (V "x"))) `alphaEquivalent` (L "y" (L "x" (V "x"))) `shouldBe` True
         it "claims that λx.x y is not alpha equivalent to λx.x" $ do
-            (Lambda "x" (App (Variable "x") (Variable "y"))) `alphaEquivalent` (Lambda "x" (Variable "x")) `shouldBe` False
+            (L "x" (A (V "x") (V "y"))) `alphaEquivalent` (L "x" (V "x")) `shouldBe` False
