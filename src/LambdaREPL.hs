720cb21827818d403f1dc4d5db49d2d1cf32ed45
Gustav Behm <gustav.behm@gmail.com>
2016-03-22 07:30:47 +0100

    Implement a simple simplifying lambda REPL

--- src/LambdaREPL.hs
+++ src/LambdaREPL.hs
@@ -1,6 +1,16 @@
 module LambdaREPL (runREPL) where
 
+import Lambda
+import LambdaParser
 import System.Console.Readline
 
 runREPL :: IO ()
-runREPL = readline "lambda> " >>= putStrLn . show
+runREPL = readline "lambda> " >>= loop
+    where
+        loop (Just s) = addHistory s >> doREPL s >> runREPL
+        loop Nothing = return ()
+
+doREPL :: String -> IO ()
+doREPL s = case parseLambda s of
+      Left e -> putStrLn e
+      Right ast -> putStrLn . show $ simplify . fromAST $ ast
